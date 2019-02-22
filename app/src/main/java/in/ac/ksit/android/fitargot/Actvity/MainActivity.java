c9db1afc100ca04956b7d6f32d8a0f93429d04d6
vivekbala <vivekbalachandra@gmail.com>
2019-02-22 14:49:03 +0530

    intial commit

--- app/src/main/java/in/ac/ksit/android/fitargot/Actvity/MainActivity.java
+++ app/src/main/java/in/ac/ksit/android/fitargot/Actvity/MainActivity.java
@@ -1,5 +1,6 @@
 package in.ac.ksit.android.fitargot.Actvity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,25 @@ import in.ac.ksit.android.fitargot.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private boolean isLoggedIn(){
+
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!isLoggedIn()){
+
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+
+        }else{
+
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/in/ac/ksit/android/fitargot/Util/LoginUtil.java
@@ -0,0 +1,7 @@
+package in.ac.ksit.android.fitargot.Util;
+
+public class LoginUtil {
+
+
+
+}
