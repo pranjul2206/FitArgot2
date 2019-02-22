b001d332be280c8f7f32e09a7ef682139c57efca
vivekbala <vivekbalachandra@gmail.com>
2019-02-22 14:31:50 +0530

    intial commit

---
+++ app/src/main/java/in/ac/ksit/android/fitargot/Actvity/LoginActivity.java
@@ -0,0 +1,21 @@
+package in.ac.ksit.android.fitargot.Actvity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import in.ac.ksit.android.fitargot.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
---
+++ app/src/main/java/in/ac/ksit/android/fitargot/Actvity/MainActivity.java
@@ -0,0 +1,15 @@
+package in.ac.ksit.android.fitargot.Actvity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.ac.ksit.android.fitargot.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
