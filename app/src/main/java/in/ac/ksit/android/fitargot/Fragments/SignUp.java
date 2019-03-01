f995fc29460302aaecea7778cf8528c135bde5b5
vivekbala <vivekbalachandra@gmail.com>
2019-03-01 22:45:20 +0530

    merged conflict

--- app/src/main/java/in/ac/ksit/android/fitargot/Fragments/SignUp.java
+++ app/src/main/java/in/ac/ksit/android/fitargot/Fragments/SignUp.java
@@ -6,16 +6,32 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidnetworking.AndroidNetworking;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 import in.ac.ksit.android.fitargot.R;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 public class SignUp extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    private CallbackManager manager;
 
     public SignUp() {
         // Required empty public constructor
@@ -47,5 +63,42 @@ public class SignUp extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+
+        manager = CallbackManager.Factory.create();
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        LoginButton fbLogin = (LoginButton) root.findViewById(R.id.login_button);
+        //fbLogin.setReadPermissions(Arrays.asList(email));
+        fbLogin.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
+//
+//        result = (TextView)findViewById(R.id.response);
+//        frs = findViewById(R.id.friends);
+
+        fbLogin.registerCallback(manager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                loginResult.getAccessToken().getUserId();
+
+
+
+            }
+
+            @Override
+            public void onCancel()
+            {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("FACEBOOK", error.toString());
+            }
+
+
+
+        });
     }
 }
